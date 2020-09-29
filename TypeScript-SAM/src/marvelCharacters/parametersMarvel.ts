8ab01c405961dab736885eee8f57664788e8e62a
LUIS CARLOS CESAR MACIEL <luis.maciel@gmail.com>
2020-09-29 09:54:28 -0300

    Update parametersMarvel.ts

--- TypeScript-SAM/src/marvelCharacters/parametersMarvel.ts
+++ TypeScript-SAM/src/marvelCharacters/parametersMarvel.ts
@@ -9,8 +9,8 @@ export class ParametersMarvel  {
 
     constructor() {
 
-        var privateKey:string = '3dabb9c6d5c8ecd4438f1a61e32019a22b9bbaf3' 
-        var apiKey:string = '28c886410b95683b349e8b5448a04e95'               
+        var privateKey:string = 'your privateKey' 
+        var apiKey:string = 'your publicKey'               
 
         var now = Date.now().toString()
 
